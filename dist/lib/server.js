af407359fe3783cda716549281ae66357c069645
Simona <simonasablinaite@gmail.com>
2024-01-05 08:22:11 +0200

    Gets home and css content

--- dist/lib/server.js
+++ dist/lib/server.js
@@ -1,5 +1,48 @@
+import http from 'node:http';
 const server = {};
+server.httpServer = http.createServer((req, res) => {
+    console.log(req.url);
+    let responseContent = '';
+    //  PRIMITYVUS BUDAS GAUTI TURINI I PUSLAPI: NAUDOTI IF'US
+    if (req.url === '/') {
+        responseContent = `<!DOCTYPE html>
+   <html lang="en">
+   
+   <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+
+      <link rel="stylesheet" href="css/main.css">
+   </head>
+   
+   <body>
+   <h1>Hello, cia yra HOME PAGE :)</h1>
+   <img src="#" alt="kazkokia nuotrauka">
+   </body>
+   
+   </html>`;
+    }
+    if (req.url === '/css/main.css') {
+        responseContent = `body,
+   body * {
+     margin: 0;
+     padding: 0;
+     box-sizing: border-box;
+     font-family: Impact, Haettenschweiler, "Arial Narrow Bold", sans-serif;
+     color: yellowgreen;
+     font-size: 30px;
+   }`;
+    }
+    if (req.url === '/favicon.ico') {
+        responseContent = 'FAVICON ICO FAILAS...';
+    }
+    return res.end(responseContent);
+});
 server.init = () => {
-    console.log('inicijuojame serveri...');
+    server.httpServer.listen(4410, () => {
+        console.log('serveris sukasi ant http://localhost:4410');
+    });
 };
 export { server };
